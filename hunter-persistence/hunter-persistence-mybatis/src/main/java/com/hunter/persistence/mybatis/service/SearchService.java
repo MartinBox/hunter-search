abed717de3de0cb9f758a18518e72a91f3807830
刘恒 <liuheng@iboxpay.com>
2020-03-21 19:21:58 +0800

    first commit

--- hunter-persistence/hunter-persistence-mybatis/src/main/java/com/hunter/persistence/mybatis/service/SearchService.java
+++ hunter-persistence/hunter-persistence-mybatis/src/main/java/com/hunter/persistence/mybatis/service/SearchService.java
@@ -1,13 +1,3 @@
-/**
- * Copyright (C) 2011-2020 ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
- * <p>
- * All right reserved.
- * <p>
- * This software is the confidential and proprietary information of iBOXCHAIN Company of China. ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall use it only in accordance with the
- * terms of the contract agreement you entered into with iBOXCHAIN inc.
- */
-
 package com.hunter.persistence.mybatis.service;
 
 /**
